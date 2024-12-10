19bdbd4e5317b086b0cc173a8327b38622e2db33
OS-SiGNas <signas13@gmail.com>
2024-12-10 03:42:06 -0400

    feat: added LOGGER_SERVICE variable for build strategy Logger instance

--- src/Applications/shared/logger-handler/make.ts
+++ src/Applications/shared/logger-handler/make.ts
@@ -1,2 +1,6 @@
-// export { Logger } from "./winston.logger.js";
-export { Logger } from "./console.logger.js";
+import { secrets } from "#Domain/config.js";
+
+//export { Logger } from "./winston.logger.js";
+//export { Logger } from "./console.logger.js";
+
+export const { Logger } = await import(`./${secrets.LOGGER_SERVICE}.logger.js`);
