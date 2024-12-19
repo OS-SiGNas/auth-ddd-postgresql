ad5adeca65ef90e939c278d1a1137c366a46606e
OS-SiGNas <signas13@gmail.com>
2024-12-19 19:31:03 -0400

    feat: implementation of a shudownGracefully function for a controlled exit of all servers, thus avoiding side effects and unfinished processes.

--- src/Domain/IServer.d.ts
+++ src/Domain/IServer.d.ts
@@ -1,5 +1,10 @@
 export interface IServer {
+	/** Main method to start listening servers*/
 	readonly start: () => Promise<void>;
-	readonly restart: () => Promise<void>;
+
+	/** All servers must implement controlled exit logic */
 	readonly stop: () => void;
+
+	/** For cases where it is necessary not to interrupt the application boot and it is more convenient to retry to start the server */
+	readonly restart: () => Promise<void>;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
 import "reflect-metadata";
-import { exit } from "node:process";
 import { servers } from "./Infrastructure/make.js";
 import { Logger } from "#shared/logger-handler/make.js";
 
 import type { IServer } from "#Domain/IServer";
 
 void (async (servers: IServer[]): Promise<void> => {
-	const logger = new Logger("Main");
-	logger.info(`Application init`);
+	const _logger = new Logger("Main");
+	_logger.info(`Application init`);
+
+	const _shutdownGracefully = (): void => {
+		for (const { stop } of servers) stop();
+		_logger.info("Exiting");
+		process.exit(1);
+	};
 
 	try {
-		for (const server of servers) await server.start();
-		logger.info("All servers started");
+		for (const { start } of servers) await start();
+		_logger.info("All servers started");
 	} catch (error) {
-		logger.error("Application crashed");
-		console.trace(error);
-		for (const server of servers) server.stop();
-		void exit(1);
+		_logger.error("Application crashed");
+		console.trace("\n", error);
+		_shutdownGracefully();
 	}
+
+	process.on("SIGINT", _shutdownGracefully);
+	process.on("SIGTERM", _shutdownGracefully);
 })(servers);
